64a34d7b3e376e01faa3151d380f0f2ece7000c2
Steve Rayner <srayner02@googlemail.com>
2015-08-03 22:05:24 +0100

    fixed issue with config path on linux

--- Module.php
+++ Module.php
@@ -49,7 +49,7 @@ class Module
     
     public function getConfig()
     {
-        return include __DIR__ . './config/module.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
    
 }
